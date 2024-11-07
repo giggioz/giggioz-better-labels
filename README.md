c9addcfb36dc99391231b64e059744da9035f796
giggioz <giggioz@gmail.com>
2024-11-07 19:58:38 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+## DEV x
+
+Run webpack in watch mode
+npx webpack --config webpack.config.js --watch
+
+Use symbolic link to local development
+
+
+## HOW TO USE
+
+Inside Foundry VTT
+
+- Add-on Modules
+- Install Module
+. In the search bar on the bottom paste this
+https://github.com/giggioz/gurps-quick-contest/releases/download/LAST_TAG/module.json
+ (LAST_TAG is the last tagged version, for example v1.0.7)
+https://github.com/giggioz/gurps-quick-contest/releases/download/v1.0.7/module.json
+
+- Activate the module inside your world
---
+++ scripts/main.js
@@ -0,0 +1,48 @@
+import { initConfig } from './config.js';
+import { registerSettings } from './settings.js';
+
+export const MODULE_ID = 'giggioz-better-labels';
+
+Hooks.on('init', () => {
+  initConfig();
+  registerSettings();
+});
+
+Hooks.on('ready', () => {
+console.log('Giggioz\'s Better Labels installed')
+});
+
+Hooks.on("createChatMessage", async (chatMessage, options, userId) => {
+  let visibility, color;
+  const isGM = game.users.get(userId)?.isGM;
+
+  if (chatMessage.blind) {
+      visibility = "Blind";
+      color = "hsl(0, 70%, 50%)";
+  } else if (chatMessage.whisper && chatMessage.whisper.length > 0) {
+      
+      if (chatMessage.whisper.length === 1 && chatMessage.whisper[0] === userId) {
+          visibility = "Self";
+          color = "hsl(300, 70%, 50%)";
+      } else {
+          visibility = "Private";
+          color = "hsl(250, 70%, 50%)";
+      }
+      // Self and private for the Master are the same thing!
+      if (isGM) {
+          visibility = 'Private/Self is Self for GMs'
+          color = "hsl(300, 70%, 50%)";
+      }
+     
+  } else {
+      visibility = "Public";
+      color = "hsl(120, 70%, 50%)";
+  }
+
+  const style = `color: white; background-color: ${color}; padding: 2px 5px; border-radius: 3px; font-size: 0.9em; margin-bottom: 5px;`;
+
+  const label = `<p style="${style}">${visibility}</p>`;
+  const newContent = label + chatMessage.content;
+
+  await chatMessage.update({ content: newContent });
+});
